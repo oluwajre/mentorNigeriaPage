c9e82a663cd29337d3a871447c7bd32690f1aef3
Ajayi Adeoluwa <oluwajre2412@gmail.com>
2024-06-13 17:56:28 +0100

    Changes made for Favicon/ index.html

--- src/LandingPage/footerSection.js
+++ src/LandingPage/footerSection.js
@@ -8,7 +8,7 @@ const FooterSection = () => {
                   
                   <div className='location'>LearnNova Learning Support Centre, A member of TreNova Group.</div>
                   <div className='location'>7B, Ogunsiji Close, Off Allen Avenue, Ikeja, Lagos.</div>
-                  <a href="tel:08179361381"><b>0817 936 1381 (Whatsapp Only)</b></a>
+                  <a href="tel:08179361381"><b>0817 936 1381 (Whatsapp-Only)</b></a>
                   <div style={{color: '#4151E5'}}><a href='https://www.learnnova.ng' target='blank'><b>www.learnnova.ng</b></a></div>
               </div>
               <div className='f-section-col phone'>
